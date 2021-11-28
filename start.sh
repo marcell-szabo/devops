21cdcccbe6707970c73fe7e76121957b7c783f65
Bence Esztergalyos <bence.esztergalyos@agcocorp.com>
2021-11-28 18:42:22 +0100

    Modify sleep time, invoke selenium test

--- start.sh
+++ start.sh
@@ -4,3 +4,5 @@ docker rmi -f ci-webapp
 docker rmi $(docker images -f "dangling=true" -q)
 cd /home/ec2-user/devops/ci
 docker-compose up --build -d --force-recreate
+sleep 40s
+docker exec -it selenium selenium-side-runner -c "browserName=chrome goog:chromeOptions.args=[headless, no-sandbox, disable-dev-shm-usage, disable-infobars]" devops.side
